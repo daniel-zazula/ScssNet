4d4e87be813d912ffe47e1d4ee34f20212204d54
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2024-05-25 13:36:06 -0300

    Added identifier parser tests

---
+++ source/ScssNet.Test/Lexing/IdentifierParserTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using ScssNet.Lexing;
+
+namespace ScssNet.Test.Lexing
+{
+	[TestClass]
+	public class IdentifierParserTests
+	{
+		[DataTestMethod]
+		[DataRow("table")]
+		[DataRow("customClass")]
+		public void ShouldParseIdentifier(string source)
+		{
+			var sourceReader = new SourceReaderMock(source);
+			var identifierParser = new IdentifierParser();
+
+			var identifierToken = identifierParser.Parse(sourceReader);
+
+			identifierToken.Should().NotBeNull();
+			identifierToken!.Text.Should().Be(source);
+			sourceReader.End.Should().BeTrue();
+		}
+	}
+}
