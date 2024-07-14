8930007df468243a7babb495048b14074bb22ca4
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2024-07-14 14:04:38 -0300

    Added dependency injection

---
+++ source/ScssNet/DependencyRegistry.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ScssNet.Lexing;
+using ScssNet.Parsing;
+
+namespace ScssNet
+{
+	internal static class DependencyRegistry
+	{
+		private static readonly IServiceCollection Services;
+
+		static DependencyRegistry()
+		{
+			Services = new ServiceCollection();
+			Services.AddReaders();
+			Services.AddParsers();
+		}
+
+		internal static IServiceProvider CreateServiceProvider(TextReader textReader)
+		{
+			Services.AddSingleton(textReader);
+			var provider = Services.BuildServiceProvider();
+			Services.RemoveAll<TextReader>();
+			return provider;
+		}
+
+		private static void AddReaders(this IServiceCollection services)
+		{
+			services.AddSingleton<SourceReader>();
+			services.AddSingleton<TokenReader>();
+		}
+
+		private static void AddParsers(this IServiceCollection services)
+		{
+			services.AddLazy<TagSelectorParser>();
+			services.AddLazy<ClassSelectorParser>();
+			services.AddLazy<IdSelectorParser>();
+			services.AddLazy<AttributteSelectorParser>();
+			services.AddLazy<CompoundSelectorParser>();
+			services.AddLazy<SubSelectorParser>();
+			services.AddLazy<SelectorListParser>();
+			services.AddLazy<ValueParser>();
+			services.AddLazy<RuleParser>();
+			services.AddLazy<BlockParser>();
+			services.AddLazy<RuleSetParser>();
+		}
+
+		private static void AddLazy<TService>(this IServiceCollection services) where TService : class
+		{
+			services.AddSingleton<TService>();
+			services.AddSingleton(sp => new Lazy<TService>(() => sp.GetRequiredService<TService>()));
+		}
+	}
+}
--- source/ScssNet/ScssCompiler.cs
+++ source/ScssNet/ScssCompiler.cs
@@ -1,7 +1,14 @@
-﻿namespace ScssNet
+﻿using System.IO;
+
+namespace ScssNet
 {
 	public class ScssCompiler
 	{
+		public string CompileSource(string source)
+		{
+			var provider = DependencyRegistry.CreateServiceProvider(new StringReader(source));
 
+			return "";
+		}
 	}
 }
