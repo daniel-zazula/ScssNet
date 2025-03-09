0a753dc4be5983c21dddff805aae86aaaade6681
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2025-03-09 11:42:10 -0300

    Added subselector to the selector parsing

--- source/ScssNet/Parsing/SelectorParser.cs
+++ source/ScssNet/Parsing/SelectorParser.cs
@@ -1,19 +1,33 @@
 ﻿using ScssNet.Lexing;
 using ScssNet.SourceElements;
+using ScssNet.Tokens;
 
 namespace ScssNet.Parsing;
 
 internal class SelectorParser
 (
-	Lazy<TagSelectorParser> tagSelectorParser, Lazy<IdSelectorParser> idSelectorParser, Lazy<ClassSelectorParser> classSelectorParser,
-	Lazy<AttributteSelectorParser> attributteSelectorParser
+	Lazy<TagSelectorParser> tagSelectorParser, Lazy<IdSelectorParser> idSelectorParser,
+	Lazy<ClassSelectorParser> classSelectorParser, Lazy<AttributteSelectorParser> attributteSelectorParser,
+	Lazy<SubSelectorParser> subSelectorParser
 )
 {
 	internal ISelector? Parse(TokenReader tokenReader)
 	{
-		return (ISelector?)tagSelectorParser.Value.Parse(tokenReader)
+		var selector = (ISelector?)tagSelectorParser.Value.Parse(tokenReader)
 			?? (ISelector?)idSelectorParser.Value.Parse(tokenReader)
 			?? (ISelector?)classSelectorParser.Value.Parse(tokenReader)
 			?? attributteSelectorParser.Value.Parse(tokenReader);
+
+		if(selector == null)
+			return null;
+
+		var separator = (IToken?)tokenReader.Match<WhiteSpaceToken>() ?? tokenReader.Match<CommentToken>();
+		if (separator != null)
+		{
+			var subSelector = (ISelector?)subSelectorParser.Value.Parse(tokenReader, selector);
+			return subSelector ?? selector;
+		}
+
+		return selector;
 	}
 }
