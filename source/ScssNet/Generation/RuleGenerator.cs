23ef9cad125ac6032a9151008cbc5dbdda95862f
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2025-10-24 17:49:47 -0300

    Implemented rule generator

---
+++ source/ScssNet/Generation/RuleGenerator.cs
@@ -0,0 +1,14 @@
+﻿using ScssNet.SourceElements;
+
+namespace ScssNet.Generation;
+
+internal class RuleGenerator(Lazy<ValueGenerator> valueGenerator)
+{
+	public void Generate(Rule rule, CssWriter writer)
+	{
+		writer.Write(rule.Property);
+		writer.Write(rule.Colon);
+		valueGenerator.Value.Generate(rule.Value, writer);
+		writer.Write(rule.SemiColon);
+	}
+}
