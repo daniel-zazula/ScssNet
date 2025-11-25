0f332685d8c4a41d9e747eaa838eb9b4ec74b547
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2025-11-25 10:48:40 -0300

    Added more attribute selector parser tests

--- source/ScssNet.Test/Parsing/AttributteSelectorParserTests.cs
+++ source/ScssNet.Test/Parsing/AttributteSelectorParserTests.cs
@@ -12,16 +12,71 @@ public class AttributteSelectorParserTests: ParserTestBase
 	[TestMethod]
 	public void ShouldParseAttributeSelector()
 	{
-		var provider = BuildServiceProvider("[type=\"text\"]");
+		var source = "[attr]";
+		var provider = BuildServiceProvider(source);
 
 		var tokenReader = provider.GetRequiredService<ITokenReader>();
 		var attributeSelectorParser = provider.GetRequiredService<AttributteSelectorParser>();
 
 		var attributeSelector = attributeSelectorParser.Parse(tokenReader);
 		attributeSelector.ShouldNotBeNull();
-		attributeSelector!.Attribute.Text.ShouldBe("type");
-		attributeSelector.Operator!.Symbol.ShouldBe(Symbol.Equals);
-		attributeSelector.Value!.Text.ShouldBe("\"text\"");
+		attributeSelector!.Attribute.Text.ShouldBe("attr");
+		attributeSelector.Operator?.ShouldBeNull();
+		attributeSelector.Value?.ShouldBeNull();
+		attributeSelector.Modifier?.ShouldBeNull();
+		attributeSelector.Qualifier.ShouldBeNull();
+		attributeSelector.Issues.ShouldBeEmpty();
+
+		tokenReader.End.ShouldBeTrue();
+	}
+
+	[DataTestMethod]
+	[DataRow(["=", Symbol.Equals])]
+	[DataRow(["~=", Symbol.ContainsWord])]
+	[DataRow(["|=", Symbol.StartsWithWord])]
+	[DataRow(["^=", Symbol.StartsWith])]
+	[DataRow(["$=", Symbol.EndsWith])]
+	[DataRow(["*=", Symbol.Contains])]
+	public void ShouldParseAttributeSelectorWithValue(string operatorString, Symbol operatorSymbol)
+	{
+		var source = $"[attr{operatorString}\"value\"]";
+		var provider = BuildServiceProvider(source);
+
+		var tokenReader = provider.GetRequiredService<ITokenReader>();
+		var attributeSelectorParser = provider.GetRequiredService<AttributteSelectorParser>();
+
+		var attributeSelector = attributeSelectorParser.Parse(tokenReader);
+		attributeSelector.ShouldNotBeNull();
+		attributeSelector!.Attribute.Text.ShouldBe("attr");
+		attributeSelector.Operator?.Symbol.ShouldBe(operatorSymbol);
+		attributeSelector.Value?.Text.ShouldBe("\"value\"");
+		attributeSelector.Modifier?.ShouldBeNull();
+		attributeSelector.Qualifier.ShouldBeNull();
+		attributeSelector.Issues.ShouldBeEmpty();
+
+		tokenReader.End.ShouldBeTrue();
+	}
+
+	[DataTestMethod]
+	[DataRow("i")]
+	[DataRow("I")]
+	[DataRow("s")]
+	[DataRow("S")]
+	public void ShouldParseAttributeSelectorWithValue(string modifier)
+	{
+		var source = $"[attr=\"value\" {modifier}]";
+		var provider = BuildServiceProvider(source);
+
+		var tokenReader = provider.GetRequiredService<ITokenReader>();
+		var attributeSelectorParser = provider.GetRequiredService<AttributteSelectorParser>();
+
+		var attributeSelector = attributeSelectorParser.Parse(tokenReader);
+		attributeSelector.ShouldNotBeNull();
+		attributeSelector!.Attribute.Text.ShouldBe("attr");
+		attributeSelector.Operator?.Symbol.ShouldBe(Symbol.Equals);
+		attributeSelector.Value?.Text.ShouldBe("\"value\"");
+		attributeSelector.Modifier?.Text.ShouldBe(modifier);
+		attributeSelector.Qualifier?.ShouldBeNull();
 		attributeSelector.Issues.ShouldBeEmpty();
 
 		tokenReader.End.ShouldBeTrue();
