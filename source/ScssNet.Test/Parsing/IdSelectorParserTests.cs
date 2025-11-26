e2ca72a9098ca25cf72e82c93a703a648634229b
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2025-11-26 15:48:47 -0300

    Added id selector parser tests

---
+++ source/ScssNet.Test/Parsing/IdSelectorParserTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ScssNet.Lexing;
+using ScssNet.Parsing;
+using Shouldly;
+
+namespace ScssNet.Test.Parsing;
+
+[TestClass]
+public class IdSelectorParserTests : ParserTestBase
+{
+	[TestMethod]
+	public void ShouldParseIdSelector()
+	{
+		var source = "#my-id";
+		var provider = BuildServiceProvider(source);
+
+		var tokenReader = provider.GetRequiredService<ITokenReader>();
+		var idSelectorParser = provider.GetRequiredService<IdSelectorParser>();
+
+		var idSelector = idSelectorParser.Parse(tokenReader);
+		idSelector.ShouldNotBeNull();
+		idSelector!.Identifier?.Text.ShouldBe("my-id");
+		idSelector.Qualifier.ShouldBeNull();
+		idSelector.Issues.ShouldBeEmpty();
+		tokenReader.End.ShouldBeTrue();
+	}
+}
