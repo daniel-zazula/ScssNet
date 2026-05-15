39bba941f807af352ffbc61c45fcda393c4c4d61
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2026-05-15 09:30:40 -0300

    Added unit parser tests

---
+++ source/ScssNet.Test/Parsing/ValueParserTests.cs
@@ -0,0 +1,54 @@
+using Microsoft.Extensions.DependencyInjection;
+using ScssNet.Lexing;
+using ScssNet.Parsing;
+using ScssNet.SourceElements;
+using ScssNet.Tokens;
+using Shouldly;
+
+namespace ScssNet.Test.Parsing;
+
+[TestClass]
+public class ValueParserTests : ParserTestBase
+{
+	[DataTestMethod]
+	[DataRow("1cm")]
+	[DataRow("2mm")]
+	[DataRow("2Q")]
+	[DataRow("3in")]
+	[DataRow("4pc")]
+	[DataRow("5pt")]
+	[DataRow("6px")]
+	[DataRow("7em")]
+	[DataRow("8rem")]
+	[DataRow("9%")]
+	public void ShouldParseUnitValue(string valueString)
+	{
+		var provider = BuildServiceProvider(valueString);
+
+		var tokenReader = provider.GetRequiredService<ITokenReader>();
+		var valueParser = provider.GetRequiredService<ValueParser>();
+
+		var value = valueParser.Parse(tokenReader);
+
+		value.ShouldNotBeNull();
+		value.ShouldBeOfType<UnitValueToken>();
+		tokenReader.End.ShouldBeTrue();
+	}
+
+	[DataTestMethod]
+	[DataRow("red")]
+	[DataRow("flex-start")]
+	public void ShouldParseIdentifierValue(string valueString)
+	{
+		var provider = BuildServiceProvider(valueString);
+
+		var tokenReader = provider.GetRequiredService<ITokenReader>();
+		var valueParser = provider.GetRequiredService<ValueParser>();
+
+		var value = valueParser.Parse(tokenReader);
+
+		value.ShouldNotBeNull();
+		value.ShouldBeOfType<IdentifierToken>();
+		tokenReader.End.ShouldBeTrue();
+	}
+}
