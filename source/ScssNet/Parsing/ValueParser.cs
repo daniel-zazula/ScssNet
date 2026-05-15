167ee934085a5abaedaec09a865f593f0d9dcf98
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2026-05-15 09:03:41 -0300

    Added Value list parsing

--- source/ScssNet/Parsing/ValueParser.cs
+++ source/ScssNet/Parsing/ValueParser.cs
@@ -8,7 +8,42 @@ internal class ValueParser
 {
 	internal IValue? Parse(ITokenReader tokenReader)
 	{
+		var value = ParseSingle(tokenReader);
+		if(value == null)
+			return null;
+
+		return ParseList(tokenReader, value) ?? value;
+	}
+
+	private IValue? ParseList(ITokenReader tokenReader, IValue firstValue)
+	{
+		var list = new List<IValue> { firstValue };
+		var lastValue = firstValue;
+		while (HasTrailingSeparator(lastValue))
+		{
+			lastValue = ParseSingle(tokenReader);
+			if (lastValue is null)
+				break;
+
+			list.Add(lastValue);
+		}
+
+		return list.Count > 1 ? new ValueList(list) : null;
+	}
+
+	private IValue? ParseSingle(ITokenReader tokenReader)
+	{
 		return tokenReader.Match<IValueToken>();
 		// TBA more comples values like function calls
+	}
+
+	private static bool HasTrailingSeparator(IValue value)
+	{
+		return value switch
+		{
+			ISeparatedToken separatedToken => separatedToken.HasTrailingSeparator(),
+			ValueList => false,
+			_ => throw new NotImplementedException("Unknow value type")
+		};
 	}
 }
---
+++ source/ScssNet/SourceElements/ValueList.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace ScssNet.SourceElements;
+
+public record ValueList : IValue
+{
+	public IReadOnlyList<IValue> Values { get; }
+
+	public SourceCoordinates Start => Values.First().Start;
+	public SourceCoordinates End => Values.Last().End;
+	public IEnumerable<Issue> Issues => Values.ConcatIssues();
+
+	internal ValueList(IReadOnlyList<IValue> values)
+	{
+		Debug.Assert(values.Count > 0, "Value list must contain at least one value");
+		Values = values;
+	}
+}
