0ba6d2c454392e052e62fdef35a1f5b0459077d7
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2026-06-16 11:50:04 -0300

    Reduced code duplication on the attribute selector tests

--- source/ScssNet.Test/Parsing/AttributeSelectorParserTests.cs
+++ source/ScssNet.Test/Parsing/AttributeSelectorParserTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ScssNet.Lexing;
 using ScssNet.Parsing;
+using ScssNet.Structures;
 using ScssNet.Tokens;
 using Shouldly;
 
@@ -13,21 +14,11 @@ public class AttributeSelectorParserTests: ParserTestBase
 	public void ShouldParseAttributeSelector()
 	{
 		var source = Selectors.AttributeSelector;
-		var provider = BuildServiceProvider(source);
 
-		var tokenReader = provider.GetRequiredService<ITokenReader>();
-		var attributeSelectorParser = provider.GetRequiredService<AttributeSelectorParser>();
-
-		var attributeSelector = attributeSelectorParser.Parse(tokenReader);
-		attributeSelector.ShouldNotBeNull();
-		attributeSelector.AssertAttributeName();
+		var attributeSelector = ShouldParseAttributeSelector(source);
 		attributeSelector.Operator?.ShouldBeNull();
 		attributeSelector.Value?.ShouldBeNull();
 		attributeSelector.Modifier?.ShouldBeNull();
-		attributeSelector.Qualifier.ShouldBeNull();
-		attributeSelector.Issues.ShouldBeEmpty();
-
-		tokenReader.End.ShouldBeTrue();
 	}
 
 	[TestMethod]
@@ -40,21 +31,11 @@ public class AttributeSelectorParserTests: ParserTestBase
 	public void ShouldParseAttributeSelectorWithValue(string operatorString, Symbol operatorSymbol)
 	{
 		var source = $"[{Selectors.AttributeName}{operatorString}{Selectors.AttributeValue}]";
-		var provider = BuildServiceProvider(source);
 
-		var tokenReader = provider.GetRequiredService<ITokenReader>();
-		var attributeSelectorParser = provider.GetRequiredService<AttributeSelectorParser>();
-
-		var attributeSelector = attributeSelectorParser.Parse(tokenReader);
-		attributeSelector.ShouldNotBeNull();
-		attributeSelector.AssertAttributeName();
+		var attributeSelector = ShouldParseAttributeSelector(source);
 		attributeSelector.Operator?.Symbol.ShouldBe(operatorSymbol);
 		attributeSelector.AssertAttributeValue();
 		attributeSelector.Modifier?.ShouldBeNull();
-		attributeSelector.Qualifier.ShouldBeNull();
-		attributeSelector.Issues.ShouldBeEmpty();
-
-		tokenReader.End.ShouldBeTrue();
 	}
 
 	[TestMethod]
@@ -65,6 +46,15 @@ public class AttributeSelectorParserTests: ParserTestBase
 	public void ShouldParseAttributeSelectorWithValue(string modifier)
 	{
 		var source = $"[{Selectors.AttributeName}={Selectors.AttributeValue} {modifier}]";
+
+		var attributeSelector = ShouldParseAttributeSelector(source);
+		attributeSelector.Operator?.Symbol.ShouldBe(Symbol.Equals);
+		attributeSelector.AssertAttributeValue();
+		attributeSelector.Modifier?.Text.ShouldBe(modifier);
+	}
+
+	private static AttributeSelector ShouldParseAttributeSelector(string source)
+	{
 		var provider = BuildServiceProvider(source);
 
 		var tokenReader = provider.GetRequiredService<ITokenReader>();
@@ -73,12 +63,11 @@ public class AttributeSelectorParserTests: ParserTestBase
 		var attributeSelector = attributeSelectorParser.Parse(tokenReader);
 		attributeSelector.ShouldNotBeNull();
 		attributeSelector.AssertAttributeName();
-		attributeSelector.Operator?.Symbol.ShouldBe(Symbol.Equals);
-		attributeSelector.AssertAttributeValue();
-		attributeSelector.Modifier?.Text.ShouldBe(modifier);
-		attributeSelector.Qualifier?.ShouldBeNull();
+		attributeSelector.Qualifier.ShouldBeNull();
 		attributeSelector.Issues.ShouldBeEmpty();
 
 		tokenReader.End.ShouldBeTrue();
+
+		return attributeSelector;
 	}
 }
