0f60ff72f74311faf09ff2a46c2a8eb31f256092
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2026-07-05 13:15:21 -0300

    Added end to end tests of main CSS syntax and features as reference

---
+++ source/ScssNet.Test/EndToEnd/EndToEndTests.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using ScssNet.Lexing;
+using ScssNet.Parsing;
+using ScssNet.Test.Parsing;
+using Shouldly;
+
+namespace ScssNet.Test.EndToEnd;
+
+[TestClass]
+public class EndToEndTests : ParserTestBase
+{
+	[TestMethod]
+	[DataRow("css1.css")]
+	[DataRow("css2.1.css")]
+	[DataRow("css3AndLater.css")]
+	public void ShouldParseCssFile(string fileName)
+	{
+		var path = Path.Combine(AppContext.BaseDirectory, "EndToEnd", fileName);
+		var source = File.ReadAllText(path);
+
+		var provider = BuildServiceProvider(source);
+
+		var sourceReader = provider.GetRequiredService<ISourceReader>();
+		var tokenReader = provider.GetRequiredService<ITokenReader>();
+		var stylesheetParser = provider.GetRequiredService<StatementParser>();
+
+		var issues = new List<Issue>();
+
+		try
+		{
+			var element = stylesheetParser.Parse(tokenReader);
+			while(element != null)
+			{
+				issues.AddRange(element.Issues);
+				element = stylesheetParser.Parse(tokenReader);
+			}
+
+			issues.ShouldBeEmpty();
+			tokenReader.End.ShouldBeTrue();
+		}
+		catch(Exception ex)
+		{
+			var lines = GetNextLines(sourceReader, 5);
+			var message = ex.Message + "\nNext lines: \n" + lines;
+			throw new Exception(message, ex);
+		}
+	}
+
+	private string GetNextLines(ISourceReader sourceReader, int lineCount)
+	{
+		var lines = new StringBuilder();
+		while(lineCount > 0)
+		{
+			var read = sourceReader.Read();
+			lines.Append(read);
+
+			if(read == char.MaxValue)
+			{
+				break;
+			}
+
+			if (read == '\r')
+			{
+				if (sourceReader.Peek() == '\n')
+				{
+					lines.Append(sourceReader.Read());
+				}
+				lineCount--;
+			}
+			else if(read == '\n')
+			{
+				lineCount--;
+			}
+		}
+		return lines.ToString();
+	}
+}
