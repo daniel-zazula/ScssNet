ae7319fb950d39861a035b6e2912190ffdd9e1ef
Daniel Santos <18038643+daniel-zazula@users.noreply.github.com>
2026-07-26 12:40:51 -0300

    Added value list generation and function call generation test

--- source/ScssNet/Generation/ValueGenerator.cs
+++ source/ScssNet/Generation/ValueGenerator.cs
@@ -24,6 +24,14 @@ internal class ValueGenerator(Lazy<FunctionCallGenerator> functionCallGenerator)
 			case FunctionCall functionCallToken:
 				functionCallGenerator.Value.Generate(functionCallToken, writer);
 				break;
+			case ValueList valueList:
+				foreach(var item in valueList.Items)
+				{
+					Generate(item.Value, writer);
+					if (item.Comma is not null)
+						writer.Write(item.Comma);
+				}
+				break;
 			default:
 				throw new InvalidOperationException($"Unsupported value type: {value.GetType().FullName}");
 		}
